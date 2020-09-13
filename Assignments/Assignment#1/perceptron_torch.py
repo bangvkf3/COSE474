3a67f83df38574becf6d1a9c0812582639b165ec
JunHyoek Bang <bangvkf3@gmail.com>
2020-09-13 21:45:02 +0900

    fix a typo

--- Assignments/Assignment#1/perceptron_torch.py
+++ Assignments/Assignment#1/perceptron_torch.py
@@ -9,7 +9,7 @@ linear = nn.Linear(3, 1)
 # torch.nn.Linear(in_features: int, out_features: int, bias: bool = True)
 
 hypothesis_step = torch.relu(torch.sign(linear(x_data)))
-hypothesis_sigmoid = torch.sigmoid(torch.sign(linear(x_data)))
+hypothesis_sigmoid = torch.sigmoid(linear(x_data))
 hypothesis_ReLU = torch.relu(linear(x_data))
 
 print(f'Step function: {hypothesis_step}')
